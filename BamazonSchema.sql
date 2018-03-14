0d91285753edd3dad35cdffcbf9bb568157f6d36
erin hubbard <erinhubbard1981@gmail.com>
2018-03-13 20:34:46 -0400

    Recognizes item and gives customer total. Not sure if posting to DB

--- BamazonSchema.sql
+++ BamazonSchema.sql
@@ -13,23 +13,25 @@ CREATE TABLE products (
 	PRIMARY KEY (id)
 );
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES ();
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Linen Duvet-White", "Bedding", 120.00, 18);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Beeswax Candle", "Home Accents", 28.00, 9);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Himalayan Salt Lamp", "Health&Wellness", 39.00, 14);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Electric Kettle", "Kitchen", 46.00, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wormwood Soup Ladle", "Kitchen", 8.00, 11);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("2x4 Jute Rug", "Rugs", 21.00, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sea Sponge Loofa", "Bath", 11.00, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Vintage Walnut Dresser", "Furniture", 1300.00, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cherry Blossom Kimono", "Lingerie", 69.00, 3);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("7oz English Rose Hand Cream", "Beauty", 18.00, 10);
+
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,82 @@
+//external npm packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var purchases = [];
+var selectedItem;
+var customerTotal;
+var updatedStockQuantity;
+
+
+//creates the connection for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon_DB"
+});
+
+//connecting to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("server connected");
+//run the start function after the connection is made to begin
+  start();
+});
+
+function start() {
+	connection.query("SELECT * FROM products", function(err, results) {
+		if (err) throw err;
+	console.log("table selected");
+	inquirer.prompt([
+		  {
+		  	name: "choice",
+		  	type: "rawlist",
+		  	choices: function() {
+		  		var choices = [];
+		  		for (var i = 0; i < results.length; i++) {
+		  			choices.push(results[i].product_name);
+		  		}
+		  		return choices;
+		  	},
+		  	message: "Welcome to Bamazon! What item do you want to purchase? (Type the number to the left of the item to select it.)"
+		  },
+		  {
+ 				name: "amount",
+		  	type: "input",
+		  	message: "How many of this item would you like to purchase?"  
+			}
+		])
+ 		.then(function(answer) {
+ 			var selectedItem;
+ 			for (var i = 0; i < results.length; i++) {
+				if (results[i].product_name === answer.choice) {
+            selectedItem = results[i];
+        }
+ 			}
+			if (selectedItem.stock_quantity > parseInt(answer.amount)) {
+ 				connection.query(
+     				"UPDATE products SET ? WHERE ?",
+      		[
+      			{stock_quantity: updatedStockQuantity
+				 	},
+					 {id: answer.choice
+						}
+					],
+					function(err) {
+              if (err) throw err;
+              updatedStockQuantity = (answer.stock_quantity - answer.amount);
+     	 		customerTotal = selectedItem.price * parseInt(answer.amount);
+					console.log("Your Item is in stock! Your total is $" + customerTotal);
+           }
+ 					);
+				}
+				else {
+      		console.log("We don't have that many in stock. Order cancelled. Select a smaller quantity or another item.");
+      		start();
+				}		
+			
+	});
+});
+}
